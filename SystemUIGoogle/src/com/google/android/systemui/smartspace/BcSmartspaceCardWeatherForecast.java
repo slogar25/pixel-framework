d4ea5df5536d3cda23b47ee520c07e5c7649117a
Oscar <osm1019@gmail.com>
2024-04-20 14:20:18 -0700

    SystemUIGoogle: Adapt for HeisenDroid

--- SystemUIGoogle/src/com/google/android/systemui/smartspace/BcSmartspaceCardWeatherForecast.java
+++ SystemUIGoogle/src/com/google/android/systemui/smartspace/BcSmartspaceCardWeatherForecast.java
@@ -81,15 +81,6 @@ public class BcSmartspaceCardWeatherForecast extends BcSmartspaceCardSecondary {
 
     @Override // com.google.android.systemui.smartspace.BcSmartspaceCardSecondary
     public final boolean setSmartspaceActions(SmartspaceTarget smartspaceTarget, BcSmartspaceDataPlugin.SmartspaceEventNotifier smartspaceEventNotifier, BcSmartspaceCardLoggingInfo bcSmartspaceCardLoggingInfo) {
-        boolean isWeatherEnabled = Settings.Secure.getIntForUser(getContext().getContentResolver(),
-                    Settings.Secure.LOCK_SCREEN_WEATHER_ENABLED,
-                    0, UserHandle.USER_CURRENT) != 0;
-        boolean isOmniWeatherEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
-                    Settings.System.LOCKSCREEN_WEATHER_ENABLED,
-                    0, UserHandle.USER_CURRENT) != 0;
-        if (!isWeatherEnabled || isOmniWeatherEnabled) {
-            return false;
-        }
         Bundle extras;
         SmartspaceAction baseAction = smartspaceTarget.getBaseAction();
         if (baseAction == null) {
