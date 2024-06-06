6514b3425d30479c3e0c561d3257536d6f810609
someone5678 <someone5678@users.noreply.github.com>
2024-06-06 21:27:26 +0900

    pixel-framework: Run extract-files
    Change-Id: Ia9c9138d9daae4f93c3f5b36cd74acfaaa6e543e
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config.mk
+++ config.mk
@@ -5,3 +5,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUIGoogle \
     SettingsGoogle
+    
+    $(call inherit-product, vendor/google/pixel-framework/common/common-vendor.mk)
